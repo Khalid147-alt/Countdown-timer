3f704684c8837726d895b0c4e1d9b8c5ac13c55c
Khalid Hussain <159793299+Khalid147-alt@users.noreply.github.com>
2024-09-11 07:53:10 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Countdown from "@/components/ui/count-down";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my First Next.js app    ..Khalid Hussain</h1>
+      <Countdown />
+      
+    </div>
+    
+  )
+
+};
